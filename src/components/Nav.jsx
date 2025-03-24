5faa003de66fde28355c7c304066d1e8e63a8ab9
fabiopacifici <boolean@fabiopacifici.com>
2025-03-24 17:38:56 +0100

    Refactor Nav component to simplify rendering of menu links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,22 @@
 
 export default function Nav({ menuLinks }) {
 
+  const linksList = menuLinks.map(link => (
+
+    <li key={link.id}>
+      <a className={link.active ? "active" : ""} href={link.url}>{link.name}</a>
+    </li>
+
+  ))
+
+  //console.log(linksList);
+
+
 
   return (
     <nav id="menu">
       <ul>
-        {
-          menuLinks.map(link => (
-
-            <li key={link.id}>
-              <a className={link.active ? "active" : ""} href={link.url}>{link.name}</a>
-            </li>
-
-          ))
-        }
+        {linksList}
       </ul>
     </nav>
   )
