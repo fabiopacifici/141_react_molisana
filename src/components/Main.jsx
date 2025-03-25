ccccd3ca5e6fcf34f30476744bf7de88476e56ba
fabiopacifici <boolean@fabiopacifici.com>
2025-03-25 11:27:14 +0100

    Remove unused button click handler and related commented code in Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,10 +3,6 @@ import ProductsList from "./ProductsList"
 export default function Main({ products }) {
 
   // logic here
-  function handleClick() {
-    alert('Button clicked!');
-
-  }
 
   // template here 
   return (
@@ -14,18 +10,6 @@ export default function Main({ products }) {
     <main>
       <ProductsList products={products} />
       {/* Demo button click */}
-
-
-
-      {/*  <button onClick={() => alert('Button clicked!')}>Click Me</button> */}
-      <button onClick={handleClick}> <i class="fas fa-heart"></i> Click Me</button>
-      <button onClick={() => { handleClick() }}></button>
-
-      {/* Never call the function directly - it is invoked when the page loads */}
-      {/*   <button onClick={alert('ciaci ao')}>Click Me</button> */}
-
-      {/* <button onClick={handleClick()}>Click Me</button> */}
-
 
 
 
