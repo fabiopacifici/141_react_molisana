17ee9564973db84447c5d48ea4cbd41bb97cead6
fabiopacifici <boolean@fabiopacifici.com>
2025-03-25 11:26:51 +0100

    Add Font Awesome stylesheet and implement button click handler in Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,13 +3,33 @@ import ProductsList from "./ProductsList"
 export default function Main({ products }) {
 
   // logic here
+  function handleClick() {
+    alert('Button clicked!');
 
+  }
 
   // template here 
   return (
 
     <main>
       <ProductsList products={products} />
+      {/* Demo button click */}
+
+
+
+      {/*  <button onClick={() => alert('Button clicked!')}>Click Me</button> */}
+      <button onClick={handleClick}> <i class="fas fa-heart"></i> Click Me</button>
+      <button onClick={() => { handleClick() }}></button>
+
+      {/* Never call the function directly - it is invoked when the page loads */}
+      {/*   <button onClick={alert('ciaci ao')}>Click Me</button> */}
+
+      {/* <button onClick={handleClick()}>Click Me</button> */}
+
+
+
+
+
     </main>
   )
 }
