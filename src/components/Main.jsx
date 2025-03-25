5d0dfeafe512e45c28cc132908ab7a73c4600727
fabiopacifici <boolean@fabiopacifici.com>
2025-03-25 12:31:39 +0100

    Add Accordion component to Main and update styles in index.css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react"
 import ProductsList from "./ProductsList"
+
 
 export default function Main({ products }) {
 
@@ -11,9 +13,66 @@ export default function Main({ products }) {
       <ProductsList products={products} />
       {/* Demo button click */}
 
-
+      <div className="container" style={{ backgroundColor: "white", padding: "20px", marginBottom: "20px" }}>
+        FAQs
+        <AccordionList />
+      </div>
 
 
     </main>
+  )
+}
+
+
+
+function AccordionList() {
+
+  // logic
+  const [isOpen, setIsOpen] = useState(1)
+
+  const accordions = [
+    { id: 1, title: 'Accordion 1', content: 'Content for Accordion 1' },
+    { id: 2, title: 'Accordion 2', content: 'Content for Accordion 2' },
+    { id: 3, title: 'Accordion 3', content: 'Content for Accordion 3' },
+    { id: 4, title: 'Accordion 4', content: 'Content for Accordion 4' },
+  ]
+  // template
+  return (
+
+    <div className="accordion" id="accordionExample">
+
+      {/* <Accordion title={'Accordion 1'} content={'Content for Accordion 1'} /> */}
+      {accordions.map((accordion, index) => (
+        <Accordion
+          key={`accordion-${accordion.id}`} title={accordion.title}
+          isOpen={isOpen === accordion.id}
+          handleOpen={() => setIsOpen(accordion.id)}
+        >
+          {accordion.content}
+        </Accordion>
+      ))}
+
+    </div>
+
+  )
+}
+
+/* isOpen */
+function Accordion({ title, isOpen, handleOpen, children }) {
+
+  // template
+  return (
+    <div className="accordion-item">
+      <h2 className="accordion-header">
+        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" onClick={handleOpen}>
+          {title}
+        </button>
+      </h2>
+      <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+        <div className="accordion-body">
+          {isOpen && children}
+        </div>
+      </div>
+    </div>
   )
 }
